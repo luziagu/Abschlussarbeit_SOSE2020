9510c1ec207b20a3589ab0e45adef2ba866076c5
luziagu <luzia.gunzenhauser@yahoo.de>
2020-07-19 13:09:07 +0200

    Canvas

--- zauberbild_canvas.js
+++ zauberbild_canvas.js
@@ -1,12 +1,20 @@
 "use strict";
 var zauberbild;
 (function (zauberbild) {
+    let canvas;
     let crc2;
+    let figures = [];
+    let backgroundColor;
+    let bigCanvas;
+    let middleCanvas;
+    let smallCanvas;
+    let backgroundIMage;
+    let save;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
-        let smallCanvas = document.querySelector("#format1");
-        let middleCanvas = document.querySelector("#format2");
-        let bigCanvas = document.querySelector("#format3");
+        smallCanvas = document.querySelector("#format1");
+        middleCanvas = document.querySelector("#format2");
+        bigCanvas = document.querySelector("#format3");
         let canvas = document.querySelector("canvas");
         if (!canvas)
             return;
@@ -15,17 +23,17 @@ var zauberbild;
         smallCanvas.addEventListener("click", chooseCanvas);
         middleCanvas.addEventListener("click", chooseCanvas);
         bigCanvas.addEventListener("click", chooseCanvas);
-        function chooseCanvas(_event) {
-            console.log("ich wurde geklickt");
-            if (smallCanvas.classList.contains("#format1")) {
-                crc2.canvas.setAttribute("height: 200px", "width: 200px");
-            }
-            if (smallCanvas.classList.contains("#format2")) {
-                crc2.canvas.setAttribute("height: 500px", "width: 500px");
-            }
-            if (smallCanvas.classList.contains("#format3")) {
-                crc2.canvas.setAttribute("height: 700px", "width: 700px");
-            }
+    }
+    function chooseCanvas(_event) {
+        console.log("ich wurde geklickt");
+        if (smallCanvas.classList.contains("#format1")) {
+            crc2.canvas.setAttribute("height: 200px", "width: 200px");
+        }
+        if (smallCanvas.classList.contains("#format2")) {
+            crc2.canvas.setAttribute("height: 500px", "width: 500px");
+        }
+        if (smallCanvas.classList.contains("#format3")) {
+            crc2.canvas.setAttribute("height: 700px", "width: 700px");
         }
     }
 })(zauberbild || (zauberbild = {}));
--- zauberbild_canvas.ts
+++ zauberbild_canvas.ts
@@ -1,15 +1,29 @@
 namespace zauberbild {
 
+    let canvas: HTMLCanvasElement;
     let crc2: CanvasRenderingContext2D; 
+    let figures: Form [] = [];
 
 
+    let backgroundColor: HTMLInputElement; 
+    let bigCanvas: HTMLInputElement; 
+    let middleCanvas: HTMLInputElement; 
+    let smallCanvas: HTMLInputElement;
+
+
+    let backgroundIMage: ImageData; 
+    let save: HTMLButtonElement; 
+    
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
 
-        let smallCanvas: HTMLInputElement = <HTMLInputElement>document.querySelector("#format1");
-        let middleCanvas: HTMLInputElement = <HTMLInputElement>document.querySelector("#format2");
-        let bigCanvas: HTMLInputElement = <HTMLInputElement>document.querySelector("#format3");
+        
+        smallCanvas = <HTMLInputElement>document.querySelector("#format1");
+    
+        middleCanvas = <HTMLInputElement>document.querySelector("#format2");
+    
+        bigCanvas = <HTMLInputElement>document.querySelector("#format3");
     
        
         let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas"); 
@@ -26,31 +40,33 @@ namespace zauberbild {
         bigCanvas.addEventListener("click", chooseCanvas); 
 
 
-        function chooseCanvas(_event: Event): void {
+       
+
+
+    }
+
+    function chooseCanvas(_event: Event): void {
  
-            console.log("ich wurde geklickt"); 
-            
-            if (smallCanvas.classList.contains("#format1")) {
-                crc2.canvas.setAttribute("height: 200px", "width: 200px"); 
-             
-    
-            }
-            
-    
-            if (smallCanvas.classList.contains("#format2")) {
-                crc2.canvas.setAttribute("height: 500px", "width: 500px"); 
-    
-            }
-            if (smallCanvas.classList.contains("#format3")) {
-                crc2.canvas.setAttribute("height: 700px", "width: 700px"); 
-    
-            }
-          
-          
-          
+        console.log("ich wurde geklickt"); 
+
+        
+        if (smallCanvas.classList.contains("#format1")) {
+            crc2.canvas.setAttribute("height: 200px", "width: 200px"); 
+       
         }
+        
 
+        if (smallCanvas.classList.contains("#format2")) {
+            crc2.canvas.setAttribute("height: 500px", "width: 500px"); 
 
+        }
+        if (smallCanvas.classList.contains("#format3")) {
+            crc2.canvas.setAttribute("height: 700px", "width: 700px"); 
+
+        }
+      
+      
+      
     }
 
    
