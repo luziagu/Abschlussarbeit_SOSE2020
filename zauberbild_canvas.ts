4aac13f0a4618d816699071095c6023505eb0a82
luziagu <luzia.gunzenhauser@yahoo.de>
2020-08-03 07:33:04 +0200

    :)

--- zauberbild_canvas.ts
+++ zauberbild_canvas.ts
@@ -239,28 +239,28 @@ namespace zauberbild {
 
                 case "triangle":
                     let position: Vector = new Vector(parseInt(info[0]), parseInt(info[1]));
-                    let triangle: Triangle = new Triangle(position, info[3]);
+                    let triangle: Triangle = new Triangle(position, info[2]);
                     triangle.draw(crcMain);
                     info = [];
                     figures.push(triangle);
                     break;
                 case "circle":
                     let positionCircle: Vector = new Vector(parseInt(info[0]), parseInt(info[1]));
-                    let circle: Triangle = new Triangle(positionCircle, info[6]);
+                    let circle: Triangle = new Triangle(positionCircle, info[2]);
                     circle.draw(crcMain);
                     info = [];
                     figures.push(circle);
                     break;
                 case "heart":
                     let positionHeart: Vector = new Vector(parseInt(info[0]), parseInt(info[1]));
-                    let heart: Triangle = new Triangle(positionHeart, info[5]);
+                    let heart: Triangle = new Triangle(positionHeart, info[2]);
                     heart.draw(crcMain);
                     info = [];
                     figures.push(heart);
                     break;
                 case "star":
                     let positionStar: Vector = new Vector(parseInt(info[0]), parseInt(info[1]));
-                    let star: Triangle = new Triangle(positionStar, info[6]);
+                    let star: Triangle = new Triangle(positionStar, info[2]);
                     star.draw(crcMain);
                     info = [];
                     figures.push(star);
